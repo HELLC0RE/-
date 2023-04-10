3b4e1e6e7773b8b8eae5d778d10ce469f85456ff
serzl <serzl@DESKTOP-8B8MU38>
2023-04-10 20:45:09 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Зачётное_задание
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox1.Text;
+                char[] strWords = { 'а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'у', 'ю', 'и' };
+                int wordsCount = 0;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (strWords.Contains(str[i]))
+                    {
+                        wordsCount++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex));
+            }                       
+        }
+    }
+}
